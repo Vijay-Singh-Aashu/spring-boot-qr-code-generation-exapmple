04bf153f2d75c0fc47b3b6f75987e82294584722
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-20 11:22:05 +0530

    initializing the project

---
+++ src/main/java/com/excellence_spring_boot_qr_code_generation/SpringBootQrCodeGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_qr_code_generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrCodeGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQrCodeGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence_spring_boot_qr_code_generation/SpringBootQrCodeGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_spring_boot_qr_code_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrCodeGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
